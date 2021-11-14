b854c580612833f87b0e72480417784d207d576a
VanyaShestakov <vanka1108@gmail.com>
2021-11-14 12:08:12 +0300

    Initial. Created project structure

---
+++ src/main/java/by/ivanshestakov/dealerstat/DealerStatApplication.java
@@ -0,0 +1,13 @@
+package by.ivanshestakov.dealerstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealerStatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/ivanshestakov/dealerstat/DealerStatApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ivanshestakov.dealerstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerStatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
